8a69425e01bc681dd290851ff3c6aafee86bee30
LyzhinIvan <lyzhin-ivan@yandex.ru>
2015-05-01 21:00:57 +0500

    Added tests on MarkdownProcessor. Solution don't work on test with incorrect nesting.

--- Kontur.Courses.Testing.Patterns/Specifications/MarkdownProcessor.cs
+++ Kontur.Courses.Testing.Patterns/Specifications/MarkdownProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
@@ -8,7 +9,18 @@ namespace Kontur.Courses.Testing.Patterns.Specifications
 	{
 		public string Render(string input)
 		{
-			throw new NotImplementedException();
+            var emReplacer = new Regex(@"([^\w\\]|^)_(.*?[^\\])_(\W|$)");
+            var strongReplacer = new Regex(@"([^\w\\]|^)__(.*?[^\\])__(\W|$)");
+            input = strongReplacer.Replace(input,
+                    match => match.Groups[1].Value +
+                            "<strong>" + match.Groups[2].Value + "</strong>" +
+                            match.Groups[3].Value);
+            input = emReplacer.Replace(input,
+                    match => match.Groups[1].Value +
+                            "<em>" + match.Groups[2].Value + "</em>" +
+                            match.Groups[3].Value);
+            input = input.Replace(@"\_", "_");
+            return input;
 		}
 	}
 
@@ -17,7 +29,62 @@ namespace Kontur.Courses.Testing.Patterns.Specifications
 	{
 		private readonly MarkdownProcessor md = new MarkdownProcessor();
 
-		//TODO see Markdown.txt
-		
+	    [Test]
+	    public void not_change_if_no_markup()
+	    {
+	        string input = "text without markup";
+	        Assert.AreEqual("text without markup", md.Render(input));
+	    }
+
+        [Test]
+        [TestCase("_a_", Result="<em>a</em>")]
+        [TestCase("a _b_", Result="a <em>b</em>")]
+        [TestCase("a_b_", Result = "a_b_")]
+        [TestCase("a _b c_", Result = "a <em>b c</em>")]
+	    public string surrroundWithEm_textInsideUnderscores(string input)
+        {
+            return md.Render(input);
+        }
+
+	    [Test]
+        [TestCase("__a__", Result = "<strong>a</strong>")]
+        [TestCase("a __b__", Result = "a <strong>b</strong>")]
+        [TestCase("a_b_", Result = "a_b_")]
+        [TestCase("a __b c__", Result = "a <strong>b c</strong>")]
+	    public string surroundWithStrong_textInsideDoubleUnderscores(string input)
+	    {
+	        return md.Render(input);
+	    }
+
+        [Test]
+        [TestCase(@"\_", Result = "_")]
+        [TestCase(@"\_a_", Result = "_a_")]
+        [TestCase(@"_a\_", Result = @"_a_")]
+	    public string ignoreUnderscore_ifEscaping(string input)
+	    {
+	        return md.Render(input);
+	    }
+
+        [Test]
+        [TestCase(@"_a __b_ c__", Result = "_a __b_ c__")]
+        [TestCase(@"__a _b__ c_", Result = "__a _b__ c_")]
+	    public string ignore_tags_if_incorrect_nesting(string input)
+	    {
+	        return md.Render(input);
+	    }
+
+	    [Test]
+        [TestCase("_a __b__ c_", Result = "<em>a <strong>b</strong> c</em>")]
+	    public string work_with_correct_nested_tags(string input)
+	    {
+            return md.Render(input);
+	    }
+
+        [Test]
+        [TestCase(@"_a __b", Result = "_a __b", TestName = "non_pair_tags")]
+        public string no_change_if_no_need(string input)
+        {
+            return md.Render(input);
+        }
 	}
 }
